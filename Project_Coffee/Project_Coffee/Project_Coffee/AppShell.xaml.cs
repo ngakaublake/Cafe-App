46787799ed35a3d66fb5e452fba6407074e0fca0
Ngakau Blake <ngakaublake1@gmail.com>
2022-10-26 12:08:31 +1300

    Initial Push

---
+++ Project_Coffee/Project_Coffee/Project_Coffee/AppShell.xaml.cs
@@ -0,0 +1,19 @@
+﻿using Project_Coffee.ViewModels;
+using Project_Coffee.Views;
+using System;
+using System.Collections.Generic;
+using Xamarin.Forms;
+
+namespace Project_Coffee
+{
+    public partial class AppShell : Xamarin.Forms.Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+            Routing.RegisterRoute(nameof(ItemDetailPage), typeof(ItemDetailPage));
+            Routing.RegisterRoute(nameof(NewItemPage), typeof(NewItemPage));
+        }
+
+    }
+}
---
+++ Project_Coffee/Project_Coffee/Project_Coffee/Views/ItemDetailPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using Project_Coffee.ViewModels;
+using System.ComponentModel;
+using Xamarin.Forms;
+
+namespace Project_Coffee.Views
+{
+    public partial class ItemDetailPage : ContentPage
+    {
+        public ItemDetailPage()
+        {
+            InitializeComponent();
+            BindingContext = new ItemDetailViewModel();
+        }
+    }
+}
